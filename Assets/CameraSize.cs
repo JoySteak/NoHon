d81aa42c49cf218bb8cbf00ea7c2f17905ed3b2c
joysteak <joysteak.gip@gmail.com>
2015-05-28 16:08:21 +0800

    Made main scene with walls

---
+++ Assets/CameraSize.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraSize : MonoBehaviour {
+
+	// Use this for initialization
+	public static CameraSize current;
+	
+	public float m_height = 0.0f;
+	public float m_width = 0.0f;
+	
+	void Awake() {
+		current = this;
+		
+		Camera cam = Camera.main;
+		m_height = 2.0f * cam.orthographicSize;
+		m_width = m_height * cam.aspect;
+	}
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/VerWallReposition.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VerWallReposition : MonoBehaviour {
+
+	public int m_wallPosition = 0;
+	
+	private bool m_repositionDone = false;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!m_repositionDone && CameraSize.current.m_width != 0.0f) {
+			Vector3 tmpPosition = gameObject.transform.position;
+			switch(m_wallPosition) {
+			case 0: tmpPosition.x = -(CameraSize.current.m_width / 2.0f);
+				break;
+			case 1: tmpPosition.x = (CameraSize.current.m_width / 2.0f);
+				break;
+			}
+			gameObject.transform.position = tmpPosition;
+			
+			m_repositionDone = true;
+		}
+	}
+}
